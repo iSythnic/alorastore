38ec1d9664cbc8ce6dba13d2810689c4fcba861b
Yuhang <68719011+yuhaislost@users.noreply.github.com>
2022-09-25 16:36:17 +1000

    Creates the UI for header component

---
+++ client/src/components/Landing.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section>
+        <div>
+            <h1>
+                <span>Powered</span>
+                <span>By Intellect</span>
+                <span>Driven By Values</span>
+            </h1>
+        </div>
+    </section>
+  )
+}
+
+export default Landing;
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head';
+import Header from '../components/Header';
+import Landing from '../components/Landing';
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>AloraStore</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <main>
+        <Landing/>
+      </main>
+    </div>
+  )
+}
+
+export default Home
